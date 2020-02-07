9a9f1036030f6c9a1cead6e49c1cd7e4fc6d1e07
Соколов Александр <Zogymac@gmail.com>
2020-02-07 19:51:50 +0300

    Solved 3 task's

--- asciimg.go
+++ asciimg.go
@@ -6,14 +6,23 @@ import (
 	"image"
 	"image/color"
 	_ "image/jpeg"
+	//"math/bits"
+
 	// Side-effect import.
 	// Сайд-эффект — добавление декодера PNG в пакет image.
 	_ "image/png"
 	"os"
 	// Внешняя зависимость.
 	"golang.org/x/image/draw"
+	"github.com/buger/goterm"
 )
-
+var (
+	output = flag.String("o", "", "file to write")
+	noscale = flag.Bool("noscale", false,"scaling")
+	width = flag.Int("w", 200, "width")
+	height = flag.Int("h", 40, "height")
+	terminalSiz = flag.Bool("trm", false, "Terminal size")
+)
 func scale(img image.Image, w int, h int) image.Image {
 	dstImg := image.NewRGBA(image.Rect(0, 0, w, h))
 	draw.NearestNeighbor.Scale(dstImg, dstImg.Bounds(), img, img.Bounds(), draw.Over, nil)
@@ -35,9 +44,8 @@ func processPixel(c color.Color) rune {
 	gc := color.GrayModel.Convert(c)
 	r, _, _, _ := gc.RGBA()
 	r = r >> 8
-
-	// symbols := []rune("@80GCLft1i;:,. ")
-	return '0'
+	symbols := []rune("@80GCLft1i;:,. ")
+	return symbols[r*uint32(len(symbols)-1)/256]
 }
 
 func convertToAscii(img image.Image) [][]rune {
@@ -54,6 +62,12 @@ func convertToAscii(img image.Image) [][]rune {
 	return textImg
 }
 
+func checkError(e error) {
+	if e != nil {
+		fmt.Println(e)
+		os.Exit(1)
+	}
+}
 func main() {
 	flag.Parse()
 
@@ -68,12 +82,29 @@ func main() {
 		fmt.Println("Error:", err.Error())
 		os.Exit(1)
 	}
-
+	o := os.Stdout
+	if *output != "" {
+		f, err := os.Create(*output)
+		defer f.Close()
+		checkError(err)
+		o = f
+	}
+	if *terminalSiz == true {
+		*width = goterm.Width()
+		*height = goterm.Height()
+	}
+	if *output == "" && *noscale == false {
+		img = scale(img, *width, *height)
+	}
+	if *noscale == true {
+		img = scale(img, *width, *height)
+	}
 	textImg := convertToAscii(img)
 	for i := range textImg {
 		for j := range textImg[i] {
-			fmt.Printf("%c", textImg[i][j])
+			fmt.Fprintf(o,"%c", textImg[i][j])
+			//Printf("%c", textImg[i][j])
 		}
-		fmt.Println()
+		fmt.Fprint(o, "\n")
 	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module asciimg
 
 go 1.12
 
-require golang.org/x/image v0.0.0-20191214001246-9130b4cfad52
+require (
+	github.com/buger/goterm v0.0.0-20181115115552-c206103e1f37
+	github.com/fatih/color v1.9.0
+	golang.org/x/image v0.0.0-20191214001246-9130b4cfad52
+	golang.org/x/sys v0.0.0-20200202164722-d101bd2416d5 // indirect
+)
